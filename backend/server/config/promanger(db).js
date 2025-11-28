9f6a701e01da20a3b789b97ee15216119181f987
shreyamahajan1 <mahajanshreya792@gmail.com>
2025-11-28 11:17:32 +0530

    updated atlast for database

--- backend/server/config/promanger(db).js
+++ backend/server/config/promanger(db).js
@@ -1,10 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongoose = require('mongoose')
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb://127.0.0.1:27017/ProManager')
-.then(()=>{
-    console.log("DB Connection Successfull");
-})
-.catch((err)=>{
-    console.log("Error in Db Connection", err);
-})
+mongoose
+  .connect(uri)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log("MongoDB Connection Error hai:", err));
